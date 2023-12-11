24c8cb994fcb94d5012abdcfcb297717e13ce816
Eric Wooley <ericwooley@gmail.com>
2023-12-10 20:01:48 -0700

    add setup script

---
+++ custom-message.md
@@ -0,0 +1,5 @@
+Make sure to do any work in the "worker" account.
+
+```bash
+sudo su - worker
+```
--- setup.sh
+++ setup.sh
@@ -14,8 +14,23 @@ sudo apt-get install -y unattended-upgrades
 
 # create a new user named "worker" with home directory
 sudo useradd -m worker
+
 # and add it to the docker group
 sudo usermod -aG docker worker
 
+# change "worker" to use zsh
+sudo chsh -s /usr/bin/zsh worker
+
+# change shell for current user
+sudo chsh -s /usr/bin/zsh $USER
+
+# add spaceship prompt
+npm install -g spaceship-prompt
+
+# make vim the default editor for worker, and my account
+
 # Setup unattended upgrades
 dpkg-reconfigure -pmedium unattended-upgrades
+
+# Add to the login message to tell user to use worker account.
+sudo cat custom-message.md >/etc/update-motd.d/99-custom-welcome
