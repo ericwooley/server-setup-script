89d6fbaadb3fb82fed87aed97f583a7b980a3c25
Eric Wooley <ericwooley@gmail.com>
2023-12-11 11:13:28 -0700

    run github actions setup.sh

--- setup.sh
+++ setup.sh
@@ -61,6 +61,7 @@ sudo chown worker:worker -R /home/worker/
 
 sudo ./give-worker-sudo.sh
 sudo -u worker /bin/bash -c "cd /home/worker && USER=worker HOME=/home/worker sudo -E ./shell-setup.sh"
+sudo -u worker /bin/bash -c "cd /home/worker && USER=worker HOME=/home/worker sudo -E ./github-actions-setup.sh"
 
 sudo ./remove-worker-sudo.sh
 
