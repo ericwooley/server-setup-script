4789d23a048b28004d8cfd5a4b72eb9de624a3b5
Eric Wooley <ericwooley@gmail.com>
2023-12-10 22:01:31 -0700

    WIP

--- setup.sh
+++ setup.sh
@@ -55,8 +55,7 @@ cd /home/worker
 echo \"Installing nvm for worker in $HOME\"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 echo \"source ~/.nvm/nvm.sh\" >>~/.zshrc
-export NVM_DIR=\\\"\$([ -z \\\"\${XDG_CONFIG_HOME-}\\\" ] && printf %s \\\"\${HOME}/.nvm\\\" || printf %s \\\"\${XDG_CONFIG_HOME}/nvm\\\")\\\"
-[ -s \\\"\$NVM_DIR/nvm.sh\\\" ] && \\. \\\"\$NVM_DIR/nvm.sh\\\" # This loads nvm
+
 mkdir -p \"\$HOME/.zsh\" &&
 git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git \"\$HOME/.zsh/spaceship\" || true
 echo 'source \$HOME/.zsh/spaceship/spaceship.zsh' >> \$HOME/.zshrc
