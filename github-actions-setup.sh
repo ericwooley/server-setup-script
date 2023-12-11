ec631e093292662039e32f733f309ebf82a05721
Eric Wooley <ericwooley@gmail.com>
2023-12-11 11:15:57 -0700

    hardcode worker

--- github-actions-setup.sh
+++ github-actions-setup.sh
@@ -9,8 +9,7 @@ fi
 # strict mode
 set -euo pipefail
 
-$HOME=/home/$USER
-cd /home/$USER
+cd /home/worker
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c
