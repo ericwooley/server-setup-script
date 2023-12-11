1ef3191e3dfacb3c30a0b581ce9482ff5361268c
Eric Wooley <ericwooley@gmail.com>
2023-12-11 11:50:47 -0700

    install latest docker

--- setup.sh
+++ setup.sh
@@ -16,6 +16,23 @@ sudo apt-get upgrade -y
 echo "Installing vim, git, and build-essential"
 sudo apt-get install -y vim git build-essential zsh
 
+echo "Installing docker"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 # install unattened upgrades
 echo "Installing unattended-upgrades"
 sudo apt-get install -y unattended-upgrades
